fbc473a4637157c019d9a2cdf11e05c724558da4
funduval <funduval@gmail.com>
2019-09-19 14:46:53 -0400

    adding model and controller scripts

---
+++ api/public/javascripts/video.js
@@ -0,0 +1,53 @@
+'use strict';
+
+
+var mongoose = require('mongoose'),
+    Video = mongoose.model('Videos');
+
+exports.list_all_videos = function(req, res) {
+  Video.find({}, function(err, tideo) {
+    if (err)
+      res.send(err);
+    res.json(tideo);
+  });
+};
+
+exports.create_a_video= function(req, res) {
+  var new_video= new Video(req.body);
+  new_video.save(function(err, video) {
+    if (err)
+      res.send(err);
+    res.json(video);
+  });
+};
+
+
+exports.read_a_video= function(req, res) {
+  Video.findById(req.params.videoId, function(err, video) {
+    if (err)
+      res.send(err);
+    res.json(video);
+  });
+};
+
+
+exports.update_a_video= function(req, res) {
+  Video.findOneAndUpdate({_id: req.params.videoId}, req.body, {new: true}, function(err, video) {
+    if (err)
+      res.send(err);
+    res.json(video);
+  });
+};
+
+
+exports.delete_a_video= function(req, res) {
+
+
+  Video.remove({
+    _id: req.params.videoId
+  }, function(err, tideo) {
+    if (err)
+      res.send(err);
+    res.json({ message: 'Video successfully deleted' });
+  });
+};
