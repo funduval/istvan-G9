b50b3dc1c56ed46b151d16225539d9a1603e3610
funduval <funduval@gmail.com>
2019-09-21 14:06:54 -0400

    add dotenv file

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
 
       callAPI() {
           fetch("http://localhost:9000/testAPI")
-              .then(res => res.text())yield
+              .then(res => res.text())
               .then(res => this.setState({ apiResponse: res }));
       }
 
